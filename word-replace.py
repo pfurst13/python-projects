bbc4284097300f834828587892b988d84460eff9
Furst Peter <furst.peter@drescher.hu>
2022-09-05 13:04:03 +0200

    upload

---
+++ word-replace.py
@@ -0,0 +1,12 @@
+from dataclasses import replace
+
+
+def replace_word():
+    
+    str = "Hi guys, i am peter, and hi hi hi hi"
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacement = input("Enter the word replacement: ")
+    print(str.replace(word_to_replace, word_replacement))
+    
+replace_word()
+    
